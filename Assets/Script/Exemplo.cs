14095c3afde5d62e57b5ce22426966f497cd5d15
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-12-05 10:54:03 -0300

    Levantar projeto

---
+++ Assets/Script/Exemplo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DragonBones; // importante
+
+public class Exemplo : MonoBehaviour
+{
+    public UnityArmatureComponent armatureComponent;
+
+    public void TouchHat(){
+        armatureComponent.animation.Play("hat", 1);
+        armatureComponent.animation.timeScale = 1.5f;
+    }
+
+    public void Dance(){
+        armatureComponent.animation.Play("dance");
+        armatureComponent.animation.timeScale = 1.5f;
+    }
+
+    public void Idle(){
+        armatureComponent.animation.Play("idle");
+        armatureComponent.animation.timeScale = 1.5f;
+    }
+
+}
