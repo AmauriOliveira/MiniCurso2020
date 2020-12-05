10a8c0d6485a1593c81c6eda62efaa30f23aaa9b
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-12-05 13:15:15 -0300

    Ajustes

--- Assets/Script/Exemplo.cs
+++ Assets/Script/Exemplo.cs
@@ -6,20 +6,24 @@ using DragonBones; // importante
 public class Exemplo : MonoBehaviour
 {
     public UnityArmatureComponent armatureComponent;
+    public float hatTimeScale = 1;
+    public float danceTimeScale = 1;
+    public float idleTimeScale = 1;
+
 
     public void TouchHat(){
         armatureComponent.animation.Play("hat", 1);
-        armatureComponent.animation.timeScale = 1.5f;
+        armatureComponent.animation.timeScale = hatTimeScale;
     }
 
     public void Dance(){
         armatureComponent.animation.Play("dance");
-        armatureComponent.animation.timeScale = 1.5f;
+        armatureComponent.animation.timeScale = danceTimeScale;
     }
 
     public void Idle(){
         armatureComponent.animation.Play("idle");
-        armatureComponent.animation.timeScale = 1.5f;
+        armatureComponent.animation.timeScale = idleTimeScale;
     }
 
 }
