7b3dd58b841c6a1e5289a167826fe7d602eb22ba
Amauri Antonio de Oliveira <amauriibate32@hotmail.com>
2020-12-05 17:51:42 -0300

    finalização do exemplo

---
+++ Assets/Script/Novo.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DragonBones; // importante
+
+public class Novo : MonoBehaviour
+{
+     public UnityArmatureComponent armatureComponent;
+
+         public void Hello(){
+        armatureComponent.animation.Play("tchau", 1);
+        armatureComponent.animation.timeScale = 1.2f;
+    }
+
+    public void Dance(){
+        armatureComponent.animation.Play("dance");
+        armatureComponent.animation.timeScale = 0.7f;
+    }
+
+}
